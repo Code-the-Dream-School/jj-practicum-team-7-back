61258d4bc92352d80e34ac0e056f14167ac0ab1b
Darya <daryapogas@gmail.com>
2025-08-22 15:13:38 -0500

    add a Challenge model

---
+++ src/models/Challenge.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+//I defined the categories array outside the schema in case we will add categories API later (to reuse the sane categories array)
+const categories = ['Fitness', 'Learning', 'Productivity', 'Health', 'Creativity', 'Self-Care', 'Finance', 'Mindfulness', 'Other'];
+
+const ChallengeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, `Let's name your challenge to get started!`],
+      minlength: [5, `That title's a bit short - try at least 5 characters`],
+      maxlength: [
+        50,
+        'Short and sweet titles work best - try under 50 characters!',
+      ],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, `Please choose a category`],
+      enum: categories,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'active', 'completed', 'failed'],
+      default: 'pending',
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    invited: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'User',
+      required: false,
+      default: [], //allow solo-challenges
+    },
+    duration: {
+      type: Number,
+      required: true,
+      min: [1, 'Duration must be at least 1 day'],
+    },
+    participant: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Ensure title is unique per creator
+ChallengeSchema.index({ title: 1, creator: 1 }, { unique: true });
+
+module.exports = mongoose.model('Challenge', ChallengeSchema);
